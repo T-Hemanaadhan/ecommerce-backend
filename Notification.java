c7e705b4572bdb614081486d4a822b619344961d
T-Hemanaadhan <hemanaadhant@gmail.com>
2025-12-12 19:43:48 +0530

    initial commit

---
+++ Notification.java
@@ -0,0 +1,20 @@
+public class Notification{
+
+
+    private String message;
+    private String recipient;
+
+    public Notification(String message, String recipient) {
+        this.message = message;
+        this.recipient = recipient;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+}
